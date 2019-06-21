db41eac3a231134e76783a66a53e287c9114c13f
hantiaotiao <1779754921@qq.com>
2019-06-21 20:54:47 +0800

    !2 change docker image name Merge pull request !2 from hantiaotiao/han

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 npm run test:unit
 npm run build
 cp -r ./django_static ./dist/static
-docker build . -t registry.cn-qingdao.aliyuncs.com/hantiaotiao/movie-kg-nginx:latest
-docker push registry.cn-qingdao.aliyuncs.com/hantiaotiao/movie-kg-nginx:latest
+docker build . -t registry.cn-qingdao.aliyuncs.com/moviekg/nginx:latest
+docker push registry.cn-qingdao.aliyuncs.com/moviekg/nginx:latest
 
 
